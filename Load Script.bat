b4a1f2cc19a5fcc3cc3f236b7f13448749550836
sunzieck77 <106163582+sunzieck77@users.noreply.github.com>
2024-01-02 18:59:01 +0700

    Update Load Script.bat

--- Load Script.bat
+++ Load Script.bat
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/sunzieck77/MyTools/main/script.bat -o "Desktop/script.bat" && start "Desktop/script.bat"
+curl https://raw.githubusercontent.com/sunzieck77/MyTools/main/script.bat -o "Desktop/script.bat" && "Desktop/script.bat"
