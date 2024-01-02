84072e3f12b663ef69aa391db424ba7f726cd17b
sunzieck77 <106163582+sunzieck77@users.noreply.github.com>
2024-01-02 18:55:38 +0700

    Update Load Script.bat

--- Load Script.bat
+++ Load Script.bat
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/sunzieck77/MyTools/main/script.bat -o "Downloads/script.bat" && "Downloads/script.bat"
+curl https://raw.githubusercontent.com/sunzieck77/MyTools/main/script.bat -o "Downloads/script.bat" && start "Downloads/script.bat"
